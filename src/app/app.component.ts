0becddf4ba5a68120c7c8af35308375b8b9d84bc
Pavan <pavankumar@tradexa.co.in>
2023-09-22 12:26:21 +0530

    Banners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'banner-1';
+  viewBanner: string = 'banner-2';
+
+  banners: string[] = [
+    "banner-1",
+    "banner-2",
+    "banner-3",
+    "banner-4",
+    "banner-5"
+  ]
+
+  changeBanner(event: any) {
+    this.viewBanner = event.target.value;
+  }
+
 }
